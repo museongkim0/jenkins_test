55e62c6baebaa21caa57c1493017b3eaa0620ef7
museongkim <mstar0228@gmail.com>
2025-03-18 10:16:34 +0900

    first commit

---
+++ backend/src/main/java/com/example/backend/TestController.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("/ex")
+    public String test() {
+        return "v2";
+    }
+}
